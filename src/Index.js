86002fe88b7ea9865c17d2521a0022f78939be27
RakaSK <rakakusuma20@gmail.com>
2022-09-15 03:02:02 +0700

    Update revisi v1.3
    Setting file untuk hosting ke HerokuApp
    - index.js -> /api diganti /
    - package.json -> name, dev, dan start nya diganti
    - .gitIgnitore -> node modules tanpa tanda /

--- src/Index.js
+++ src/Index.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const port = process.env.PORT || 7070; 
 
 
-app.get("/api", (req, res)=> res.send("Koneksi Anda Berhasil !!"))
+app.get("/", (req, res)=> res.send("Koneksi Anda Berhasil !!"))
 
 
 app.listen( port, () => console.log('Listen on port ' + process.env.PORT) );
